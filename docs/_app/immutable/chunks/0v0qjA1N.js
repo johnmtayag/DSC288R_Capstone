087f91d14c5e792b4a552c01417a1dadc507400f
Zoey Zhao <68380548+yepzoey@users.noreply.github.com>
2025-02-23 00:36:59 -0800

    Fix: Corrected base path for GitHub Pages

---
+++ docs/_app/immutable/chunks/0v0qjA1N.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_2ueree)==null?void 0:s.base)??"/DSC288R_Capstone";var e;const t=((e=globalThis.__sveltekit_2ueree)==null?void 0:e.assets)??a;export{t as a,a as b};
